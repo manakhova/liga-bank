9eb13fcf569ff13b738474af3a78a311a20536c2
Tatiana Manakhova <tat.manakhova@gmail.com>
2021-07-15 21:17:10 +0300

    Начинает верстку

---
+++ src/components/app.jsx
@@ -0,0 +1,17 @@
+import '../scss/App.scss';
+import React from 'react';
+import Header from './header';
+import Main from './main';
+import Footer from './footer';
+
+const App = () => {
+  return (
+    <>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </>
+  );
+};
+
+export default App;
